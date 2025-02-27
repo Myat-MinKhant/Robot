0359174df76a9efb5c376bff4b86278dda19c358
Myat-MinKhant <125357767+Myat-MinKhant@users.noreply.github.com>
2025-02-27 20:58:15 +0630

    Add

---
+++ utilities/pdf_load_text.py
@@ -0,0 +1,7 @@
+def load_text(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        print("Error: Text file not found.")
+        return ""
